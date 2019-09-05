3ed3ec1c4c6f48c9b16645c180a6500ed4e1360e
JustinRlle <justin.rerolle@protonmail.com>
2019-09-05 14:32:45 +0200

    convert from tokio/hyper to async-std/isahc

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-#![feature(async_await, error_iter)]
-
 mod error;
 
+use async_std::{fs, task};
 use std::{
     ops::Range,
     path::{Path, PathBuf},
 };
 
-use futures_util::{future::join_all, StreamExt};
-use hyper::header;
+use futures::future::try_join_all;
 
-static USER_AGENT: &str = concat!("Paraget/", env!("CARGO_PKG_VERSION"));
+static USER_AGENT: &str = concat!("zrglng/", env!("CARGO_PKG_VERSION"));
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-type HttpsClient = hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>;
 
 #[derive(Debug, Clone)]
 struct FileInfo {
@@ -22,10 +19,10 @@ struct FileInfo {
 }
 
 #[derive(Debug, Clone)]
-struct Options<'a> {
+struct Options {
     parts: u64,
-    url: &'a str,
-    dest: &'a Path,
+    url: String,
+    dest: PathBuf,
 }
 
 #[derive(Debug, Clone)]
@@ -34,23 +31,24 @@ struct RangeQuery {
     idx: u64,
 }
 
-async fn file_info(client: &HttpsClient, url: &str) -> Result<FileInfo> {
-    let req = hyper::Request::head(url)
-        .header(header::USER_AGENT, USER_AGENT)
-        .body(hyper::Body::empty())?;
-    let res = client.request(req).await?;
+async fn file_info(url: &str) -> Result<FileInfo> {
+    let req = http::Request::head(url)
+        .header(http::header::USER_AGENT, USER_AGENT)
+        .body(())?;
+    let res = isahc::send_async(req).await?;
+
     if !res.status().is_success() {
         bail!("invalid status code");
     }
     let content_length = res
         .headers()
-        .get(header::CONTENT_LENGTH)
+        .get(http::header::CONTENT_LENGTH)
+        .and_then(|h| h.to_str().ok())
         .ok_or_else(|| err!("no content type"))?
-        .to_str()?
         .parse::<u64>()?;
     let supports_range = res
         .headers()
-        .get(header::ACCEPT_RANGES)
+        .get(http::header::ACCEPT_RANGES)
         .map(|v| v == "bytes")
         .unwrap_or(false);
     Ok(FileInfo {
@@ -59,12 +57,7 @@ async fn file_info(client: &HttpsClient, url: &str) -> Result<FileInfo> {
     })
 }
 
-async fn partial_get(
-    client: &HttpsClient,
-    url: &str,
-    dest: &Path,
-    range: RangeQuery,
-) -> Result<(u64, PathBuf)> {
+async fn partial_get(url: &str, dest: &Path, range: RangeQuery) -> Result<(u64, PathBuf)> {
     let mut tmp_path = PathBuf::from(dest);
     let filename = {
         let mut filename = std::ffi::OsString::from(".");
@@ -76,76 +69,63 @@ async fn partial_get(
         filename.push(format!(".part-{}", range.idx));
         filename
     };
-    dbg!(&filename, range.idx);
+    log::info!("downloading part {} at {:?}", range.idx, &filename);
     tmp_path.set_file_name(&filename);
     let range_header = format!("bytes={}-{}", range.range.start, range.range.end - 1);
     let range_header: &str = range_header.as_ref();
-    let req = hyper::Request::get(url)
-        .header(header::USER_AGENT, USER_AGENT)
-        .header(header::RANGE, range_header)
-        .body(hyper::Body::empty())?;
-    let res = client.request(req).await.map_err(|e| {
-        err_of!(
-            e,
-            "failed to do patial get of following bytes: {:?}",
-            range.range
-        )
-    })?;
+
+    let req = http::Request::get(url)
+        .header(http::header::USER_AGENT, USER_AGENT)
+        .header(http::header::RANGE, range_header)
+        .body(())?;
+
+    let mut res = isahc::send_async(req)
+        .await
+        .map_err(|e| err_of!(e, "failed partial get for range: {:?}", range.range))?;
+
     if !res.status().is_success() {
         bail!("invalid status code");
     }
-    eprintln!("creating file");
-    let file = tokio::fs::File::create(PathBuf::from(&tmp_path))
+    log::debug!("creating file");
+    let mut file = fs::File::create(PathBuf::from(&tmp_path))
         .await
         .map_err(|e| err_of!(e, "failed to create tmp file at {}", &tmp_path.display()))?;
-    eprintln!("copying chunks from req to file");
-    let file = res
-        .into_body()
-        .fold(Ok(file), |try_file, chunk| write_chunk(chunk, try_file))
+    log::debug!("copying chunks from req to file");
+    futures::io::AsyncReadExt::copy_into(res.body_mut(), &mut file)
         .await
         .map_err(|e| err_of!(e, "failed to write file"))?;
-    eprintln!("finished downloading part {}", range.idx);
+
+    log::debug!("finished downloading part {}", range.idx);
     Ok((range.idx, tmp_path))
 }
 
-async fn write_chunk(
-    chunk: hyper::Result<hyper::Chunk>,
-    try_file: Result<tokio::fs::File>,
-) -> Result<tokio::fs::File> {
-    if let Ok(mut file) = try_file {
-        let bytes = chunk?.into_bytes();
-        dbg!(bytes.len());
-        use tokio::io::AsyncWriteExt;
-        file.write_all(&bytes)
-            .await
-            .map_err(|e| err_of!(e, "failed to write a chunk"))?;
-        Ok(file)
-    } else {
-        try_file
-    }
-}
-
-async fn parallel_get(client: &HttpsClient, opts: &Options<'_>) -> Result<()> {
-    let file_info = file_info(client, opts.url)
+async fn parallel_get(opts: &Options) -> Result<()> {
+    let file_info = file_info(opts.url.as_str())
         .await
         .map_err(|e| err_of!(e, "failed to do HEAD req"))?;
-    dbg!(&file_info);
+    log::info!("file size: {}, supports range: {}", &file_info.content_length, &file_info.supports_range);
     let parts = if file_info.supports_range {
         opts.parts
     } else {
         1
     };
     let ranges = get_ranges(file_info.content_length, parts);
-    dbg!(ranges.clone().collect::<Vec<_>>());
+    log::trace!("ranges: {:?}", ranges.clone().collect::<Vec<_>>());
 
-    let partial_reqs = ranges.map(|range| partial_get(client, opts.url, opts.dest, range));
+    let partial_reqs = ranges.map(|range| {
+        let opts: Options = opts.clone();
+        task::spawn(async move { partial_get(opts.url.as_str(), &opts.dest, range).await })
+    });
 
-    let paths = join_all(partial_reqs).await;
-    let paths = paths.into_iter().collect::<Result<Vec<_>>>()?;
-    //let mut out_file = tokio::fs::File::create(opts.dest).await?;
-    for (idx, path) in paths.iter() {
-        dbg!(idx, path.display());
-        // out_file.write_all(body.as_ref())?;
+    let mut files = try_join_all(partial_reqs)
+        .await
+        .map_err(|e| err_of!(e, "failed to download a part"))?;
+    let mut out_file = fs::File::create(&opts.dest).await?;
+    files.sort_unstable_by_key(|&(idx, _)| idx);
+    for (_, path) in files {
+        let file = fs::File::open(&path).await?;
+        futures::io::AsyncReadExt::copy_into(file, &mut out_file).await?;
+        fs::remove_file(&path).await?;
     }
     Ok(())
 }
@@ -167,12 +147,12 @@ fn get_ranges(content_length: u64, parts: u64) -> impl Iterator<Item = RangeQuer
     })
 }
 
-fn dest_from_url(url: &url::Url) -> &std::path::Path {
+fn dest_from_url(url: &url::Url) -> PathBuf {
     if let Some(segments) = url.path_segments() {
         let last_segment = segments.last().unwrap_or("index.html");
-        std::path::Path::new(last_segment)
+        PathBuf::from(last_segment)
     } else {
-        std::path::Path::new("index.html")
+        PathBuf::from("index.html")
     }
 }
 
@@ -180,29 +160,24 @@ async fn run() -> Result<()> {
     let target_url = "http://i.redd.it/f61r13m3k2931.jpg";
     let url = url::Url::parse(target_url)?;
     let opts = Options {
-        parts: 1,
-        url: target_url,
+        parts: 2,
+        url: target_url.to_owned(),
         dest: dest_from_url(&url),
     };
-    let https = hyper_tls::HttpsConnector::new(4)?;
-    let client = hyper::Client::builder().build(https);
-    parallel_get(&client, &opts).await?;
+    parallel_get(&opts).await?;
     Ok(())
 }
 
 fn main() {
-    env_logger::init();
-    tokio_main();
-}
-
-#[tokio::main]
-async fn tokio_main() {
-    if let Err(err) = run().await {
-        eprintln!("Error: {}", err);
-        let sources = std::iter::successors(err.source(), |err| err.source());
-        for source in sources {
-            eprintln!("  caused by: {}", source);
+    pretty_env_logger::init_timed();
+    task::block_on(async {
+        if let Err(err) = run().await {
+            eprintln!("Error: {}", err);
+            let sources = std::iter::successors(err.source(), |err| err.source());
+            for source in sources {
+                eprintln!("  caused by: {}", source);
+            }
+            std::process::exit(1);
         }
-        std::process::exit(1);
-    }
+    })
 }
